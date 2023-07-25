b8df354d960b10d7b7c756de1ea60a509eafbe57
pragun-bansal <pragun.bansal@yahoo.in>
2023-07-26 01:01:17 +0530

    reviews

--- src/Components/RatingSystem/RatingGraph.jsx
+++ src/Components/RatingSystem/RatingGraph.jsx
@@ -15,6 +15,7 @@ const RatingGraph = ({product}) => {
         Number(ratingWith1) /
         Number(product.reviews.length)
       ).toFixed(2) * 100
+
       const ratingWith2 = product.reviews.reduce((count, review) => {
         if (review.rating === 2) {
           return count + 1;
@@ -59,27 +60,23 @@ const RatingGraph = ({product}) => {
         Number(ratingWith5) /
         Number(product.reviews.length)
       ).toFixed(2) * 100
-
-
-        const totalStar = (((ratingWith1*1)+(ratingWith2*2)+(ratingWith3*3)+(ratingWith4*4)+(ratingWith5*5))/product.reviews.length).toFixed(2)
-        const stars = Math.round(totalStar)
     return (
         <div className='lg:w-[50vw] ml-4 mt-16'>
             {/* <h1 className='justify-center text-center'>Customer Reviews</h1> */}
 
             <div class="flex items-center mb-2 ">
-            {[...Array(stars)].map((e, i) => (
+            {[...Array(Math.round(product.rating))].map((e, i) => (
                 <svg class="w-4 h-4 text-[#EE6983] mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                 </svg>
                                         ))}
-            {[...Array(5-stars)].map((e, i) => (
+            {[...Array(5-Math.round(product.rating))].map((e, i) => (
                 <svg class="w-4 h-4 text-gray-300 mr-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                 </svg>
                 ))}
                 
-                <p class="ml-2 text-sm font-medium text-gray-400 dark:text-gray-500">{totalStar} out of 5</p>
+                <p class="ml-2 text-sm font-medium text-gray-400 dark:text-gray-500">{product.rating} out of 5</p>
             </div>
             <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{product.reviews.length} global ratings</p>
             <div class="flex items-center mt-4">
