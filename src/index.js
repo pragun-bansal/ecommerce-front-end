d1827a2c385050f18f55cda94c81d71f6685cf2e
pragun-bansal <pragun.bansal@yahoo.in>
2023-09-24 02:58:54 +0530

    adding google analytics

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import { Provider } from 'react-redux';
+// import store from './Redux/Store/store';
+// import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}>
+//   <Provider store={store}>
+//   <React.StrictMode>
+//     <App />
+//     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" ></script>
+//   </React.StrictMode>
+//   </Provider>
+//   </GoogleOAuthProvider>
+  
+// );
+
+
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,15 +28,20 @@ import { Provider } from 'react-redux';
 import store from './Redux/Store/store';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+// Import react-ga
+import ReactGA from 'react-ga';
+
+// Initialize react-ga with your Tracking ID
+ReactGA.initialize('UA-XXXXXXXXX-X'); // Replace with your Tracking ID
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}>
-  <Provider store={store}>
-  <React.StrictMode>
-    <App />
-    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" ></script>
-  </React.StrictMode>
-  </Provider>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+      </React.StrictMode>
+    </Provider>
   </GoogleOAuthProvider>
-  
 );
